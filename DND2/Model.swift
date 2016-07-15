c8b6b91f49b03607399b33f9326520730750a6ab
AbbyEF109 <abbyef109@gmail.com>
2016-07-15 13:22:06 -0400

    Half a day's progress on Do Not Disturb, currently has data picker and button, working on making alarm notify and adding permission access popup for next commit

---
+++ DND2/Model.swift
@@ -0,0 +1,28 @@
+//
+//  RealmHelper.swift
+//  DoNotDisturb
+//
+//  Created by Abby on 7/14/16.
+//  Copyright © 2016 AbbyEF109. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Alarm {
+    var fireDate: NSDate
+    var timeZone: NSDate
+    var repeatInterval: Int
+    var soundName: String
+    var alertBody: String
+    
+    init (fireDate: NSDate, timeZone: NSDate, repeatInterval: Int, soundName: String, alertBody: String) {
+        self.fireDate = fireDate
+        self.timeZone = timeZone
+        self.repeatInterval = repeatInterval
+        self.soundName = soundName
+        self.alertBody = alertBody
+        
+        
+    }
+}
--- DND2/ViewController.swift
+++ DND2/ViewController.swift
@@ -1,25 +1,79 @@
 //
 //  ViewController.swift
-//  DND2
+//  DoNotDisturb
 //
-//  Created by Abby on 7/14/16.
+//  Created by Abby on 7/12/16.
 //  Copyright © 2016 AbbyEF109. All rights reserved.
 //
 
 import UIKit
+//import RealmSwift
+//import AudioToolbox
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var setAlarm: UIButton!
+    @IBOutlet var datePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        datePicker.datePickerMode = .DateAndTime
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        //registering for sending user various kinds of notifications
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound|UIUserNotificationType.Alert |UIUserNotificationType.Badge, categories: nil)
+            // Override point for customization after application launch.
+            return true
+    }
+    
+    @IBAction func alarmButtonTapped(sender: UIButton) {
+        scheduleAlarmForDate(datePicker.date)
+    }
+    
+    @IBAction func datePickerSelected(sender: UIDatePicker) {
+        //Code to save alarm
+        
+        
+        
+        
+    }
 
+    func scheduleAlarmForDate(theDate: NSDate) {
+        let app: UIApplication = UIApplication.sharedApplication()
+        let oldNotifications: [UILocalNotification] = app.scheduledLocalNotifications!
+        // Clear out the old notification before scheduling a new one.
+        if oldNotifications.count > 0 { app.cancelAllLocalNotifications() }
+        // Create a new notification.
+        let alarm = UILocalNotification()
+        alarm.fireDate = theDate
+        alarm.timeZone = NSTimeZone.defaultTimeZone()
+        alarm.repeatInterval = NSCalendarUnit(rawValue: 0)
+        alarm.soundName = "alarmsound.caf"
+        alarm.alertBody = "Time to wake up!"
+        app.scheduleLocalNotification(alarm)
+        print("setting alarm")
+        
+    }
+    
+    
+    
+    
 
+    // Mute/unmute
+    //var value: Float = 0.0
+    //AudioSessionSetProperty(kAudioSessionProperty_CurrentHardwareOutputVolume, sizeof(), value)
+    
+    
+    //Alarm tones to come later
+    
+    
 }
 
