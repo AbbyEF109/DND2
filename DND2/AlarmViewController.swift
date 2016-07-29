8ebb1449d8351912c6eeaa35bd7c2828d82d8078
AbbyEF109 <abbyef109@gmail.com>
2016-07-29 16:52:19 -0400

    Last changes based on user testing. App pretty much fully functional. Need to fix 14 second problem and UI.

--- DND2/AlarmViewController.swift
+++ DND2/AlarmViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ViewController.swift
 //  DoNotDisturb
@@ -23,8 +24,8 @@ class AlarmViewController: UIViewController {
     var soundApp = AVAudioPlayer()
     let notification = UILocalNotification()
     
-
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -33,10 +34,10 @@ class AlarmViewController: UIViewController {
         self.datePicker.datePickerMode = .DateAndTime
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.fireAlarm), name: "AlarmNotification", object: nil)
         /*let alertController = UIAlertController(title: "Welcome!", message:"Set a time and hit the Set Alarm button..", preferredStyle: UIAlertControllerStyle.Alert)
-        let cancel = UIAlertAction(title: "Got it", style: UIAlertActionStyle.Cancel, handler: nil)
-        alertController.addAction(cancel)
-        self.presentViewController(alertController, animated: true, completion: nil)*/
-
+         let cancel = UIAlertAction(title: "Got it", style: UIAlertActionStyle.Cancel, handler: nil)
+         alertController.addAction(cancel)
+         self.presentViewController(alertController, animated: true, completion: nil)*/
+        
     }
     
     
@@ -45,39 +46,40 @@ class AlarmViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func openSettingsButtonPressed(sender: AnyObject) {
-//        let URL: NSURL = NSURL(string: UIApplicationOpenSettingsURLString)!
+    @IBAction func openSettingsButtonPressed(sender: UIButton) {
+        //        let URL: NSURL = NSURL(string: UIApplicationOpenSettingsURLString)!
         let URL: NSURL = NSURL(string: "prefs:root=General")!
         UIApplication.sharedApplication().openURL(URL)
-        
+    
+    
         
         /*let alertController = UIAlertController(title: "Almost done!", message:"Set your phone to Airplane Mode and lock it. The app will do the rest! Rest well!", preferredStyle: UIAlertControllerStyle.Alert)
-        let cancel = UIAlertAction(title: "Got it", style: UIAlertActionStyle.Cancel, handler: nil)
-        alertController.addAction(cancel)
-        self.presentViewController(alertController, animated: true, completion: nil)
-        let URL: NSURL = NSURL(string: "prefs:root=General")!
-        UIApplication.sharedApplication().openURL(URL)
-        let alertController = UIAlertController(title: "Almost Done!", message:"Turn on Airplane Mode. Only the notifications from this app will go off. Rest well!", preferredStyle: UIAlertControllerStyle.Alert)
-        self.presentViewController(alertController, animated: true, completion: nil)*/
+         let cancel = UIAlertAction(title: "Got it", style: UIAlertActionStyle.Cancel, handler: nil)
+         alertController.addAction(cancel)
+         self.presentViewController(alertController, animated: true, completion: nil)
+         let URL: NSURL = NSURL(string: "prefs:root=General")!
+         UIApplication.sharedApplication().openURL(URL)
+         let alertController = UIAlertController(title: "Almost Done!", message:"Turn on Airplane Mode. Only the notifications from this app will go off. Rest well!", preferredStyle: UIAlertControllerStyle.Alert)
+         self.presentViewController(alertController, animated: true, completion: nil)*/
     }
     
     
     /*func delay(bySeconds seconds: Double, dispatchLevel: DispatchLevel = .Main, closure: () -> Void) {
-        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
-        dispatch_after(dispatchTime, dispatchLevel.dispatchQueue, closure)
-    }
-    
-    enum DispatchLevel {
-        case Main, UserInteractive, UserInitiated, Utility, Background
-        var dispatchQueue: OS_dispatch_queue {
-            switch self {
-            case .Main:             return dispatch_get_main_queue()
-            case .UserInteractive:  return dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)
-            case .UserInitiated:    return dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
-            case .Utility:          return dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)
-            case .Background:       return dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0) }
-        }
-    }*/
+     let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, Int64(seconds * Double(NSEC_PER_SEC)))
+     dispatch_after(dispatchTime, dispatchLevel.dispatchQueue, closure)
+     }
+     
+     enum DispatchLevel {
+     case Main, UserInteractive, UserInitiated, Utility, Background
+     var dispatchQueue: OS_dispatch_queue {
+     switch self {
+     case .Main:             return dispatch_get_main_queue()
+     case .UserInteractive:  return dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0)
+     case .UserInitiated:    return dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
+     case .Utility:          return dispatch_get_global_queue(QOS_CLASS_UTILITY, 0)
+     case .Background:       return dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0) }
+     }
+     }*/
     
     @IBAction func alarmButtonTapped(sender: UIButton) {
         self.scheduleNotificationForDate(datePicker.date)
@@ -91,12 +93,12 @@ class AlarmViewController: UIViewController {
         //reference to NSUserDefaults (instance of the class)
         //WIP
         /*let defaults = NSUserDefaults.standardUserDefaults()
-        print("This is from NSUserdefaults")
-        defaults.setObject(NSDate(), forKey: "Date")
-        let pickedDate = defaults.objectForKey("Date") as? NSDate ?? NSDate()
-        print(pickedDate)
-        
-        print("Ends NSUserDefaults")*/
+         print("This is from NSUserdefaults")
+         defaults.setObject(NSDate(), forKey: "Date")
+         let pickedDate = defaults.objectForKey("Date") as? NSDate ?? NSDate()
+         print(pickedDate)
+         
+         print("Ends NSUserDefaults")*/
         
         //defaults.setObject(NSDate(), forKey: "Date")
         //let getDate = defaults.objectForKey("Date") as? NSDate ?? NSDate()
@@ -107,26 +109,26 @@ class AlarmViewController: UIViewController {
          let array_get = defaults.objectForKey("Name") as? [String] ?? [String]()
          print(array_get)
          prints an empty array
-        defaults.setObject(NSDate(), forKey: "Date")
-        let getDate = defaults.objectForKey("Date") as? [String] ?? [String]()
-        defaults.synchronize()
-        print(getDate)
-        prints empty array -_-;
-        let dict = ["Name": "iOS-Blog", "tagline": "Awesome Tutorials", "rating": "10/10"]
-        defaults.setObject(dict, forKey: "SavedDict")
-        let savedDict = defaults.objectForKey("SavedDict") as? [String: String] ?? [String: String]()
-        print(savedDict)
-        well at least the example works... */
+         defaults.setObject(NSDate(), forKey: "Date")
+         let getDate = defaults.objectForKey("Date") as? [String] ?? [String]()
+         defaults.synchronize()
+         print(getDate)
+         prints empty array -_-;
+         let dict = ["Name": "iOS-Blog", "tagline": "Awesome Tutorials", "rating": "10/10"]
+         defaults.setObject(dict, forKey: "SavedDict")
+         let savedDict = defaults.objectForKey("SavedDict") as? [String: String] ?? [String: String]()
+         print(savedDict)
+         well at least the example works... */
         /*let aDict: Dictionary = ["fireDate": NSDate, "timeZone": NSDate, "repeatInterval": Int, "soundName": String, "alertBody": String]
-        defaults.setObject(aDict, forKey: "alarmDict")
-        let dict = defaults.objectForKey("alarmDict") as? [String: String] ?? [String: String]()
-        print(dict)*/
+         defaults.setObject(aDict, forKey: "alarmDict")
+         let dict = defaults.objectForKey("alarmDict") as? [String: String] ?? [String: String]()
+         print(dict)*/
         //ARGH
         /*defaults.setObject(NSDate(), forKey: "now")
-        let getNow = defaults.objectForKey("now") as? [String] ?? [String]()
-
-        print(getNow)
-        */
+         let getNow = defaults.objectForKey("now") as? [String] ?? [String]()
+         
+         print(getNow)
+         */
         //Let's try this again...
         //let DatePickerDate: NSDate = datePicker.date
         //defaults.setObject(NSDate(), forKey: "DatePickerDate")
@@ -136,8 +138,8 @@ class AlarmViewController: UIViewController {
         //default.setObject(datePicker.date, forKey date)
         //Says default can only be used in switch statement for some reason
         /*func set(_: anyObject? forKey: String){
-            defaults.setObject(datePicker.date, forKey: datePickerDate)
-        } Also not working*/
+         defaults.setObject(datePicker.date, forKey: datePickerDate)
+         } Also not working*/
         
     }
     
@@ -164,93 +166,93 @@ class AlarmViewController: UIViewController {
         
     }
     
-   // func alarmForDate(pickedDate: NSDate) {
-        //let defaults = NSUserDefaults.standardUserDefaults()
-        //let pickedDate = datePicker.date
-        //print("This is the picked date:")
-        //print(pickedDate)
-        //print("This is the notification date:")
-        //let setDate = notification.fireDate
-        //print(setDate)
-        //print("This is pickedDate")
-        //print(pickedDate)
-        //print("This is from NSUserdefaults")
-        //defaults.setObject(pickedDate, forKey: "savedPickedDate")
-        //let sPD = defaults.objectForKey("savedPickedDate") as? NSDate ?? NSDate()
-        //print(sPD)
-        //print("Ends NSUserDefaults")
-        
-        //let now = NSDate()
-        //print("This is now")
-        //print(now)
-        
-        /*while(sPD != now){
-            sleep(1)
-        }*/
-        
-        /*while(sPD != now){
-            var timer = NSTimer()
-            let delay = 0.1
-            timer = NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: #selector(delayedAction), userInfo: nil, repeats: false)
-        }*/
-        
-        /*while(sPD != now){
-            
-        }*/
-        
-        //print("PickedDate works!")
-        
-        /*while(sPD != now){
-         sleep(1)
-         }*/
-        
-        /*while (sPD != now){
-         delay(bySeconds: 1, dispatchLevel: .Background) {
-         while (sPD = now){
-         
-         }
-         }
-         }*/
-        /*while (sPD != now){
-         //var delayTimer = NSTimer()
-         let myTimer : NSTimer = NSTimer.scheduledTimerWithTimeInterval(, target: self, selector: Selector("alarmForDate"), userInfo: nil, repeats: false)
-         
-         }*/
-        
-//        let defaults = NSUserDefaults.standardUserDefaults()
-//        let pickedDate = datePicker.date
-//        defaults.setObject(pickedDate, forKey: "savedPickedDate")
-//        let sPD = defaults.objectForKey("savedPickedDate") as? NSDate ?? NSDate()
-//        let now = NSDate()
-        
-        
-//        
-//        let path = "/System/Library/Audio/UISounds/dtmf-3.caf"
-//        //let path = NSBundle.mainBundle().pathForResource("tickle", ofType: "mp3")
-//        let url = NSURL(fileURLWithPath: path)
-//        
-//        do {
-            //let soundApp = try AVAudioPlayer(contentsOfURL: url)
-//            self.soundApp = try AVAudioPlayer(contentsOfURL: url)
-//            self.soundApp.play()
-//            let testSoundApp = self.soundApp.play()
-//            print(testSoundApp)
-//            self.soundApp.numberOfLoops = 3
-//            print("sounds play now yay")
-//            let alertController = UIAlertController(title: "Are you up now?", message:"Do you want to turn off the ringtone?", preferredStyle: UIAlertControllerStyle.Alert)
-//            let cancel = UIAlertAction(title: "Yes", style: UIAlertActionStyle.Cancel, handler: nil)
-//            alertController.addAction(cancel)
-//            self.presentViewController(alertController, animated: true, completion: nil)
-//        }
-//            
-//            
-//        catch {
-//            print("There was an error accessing the sound.")
-//        }
-//    }
-
+    // func alarmForDate(pickedDate: NSDate) {
+    //let defaults = NSUserDefaults.standardUserDefaults()
+    //let pickedDate = datePicker.date
+    //print("This is the picked date:")
+    //print(pickedDate)
+    //print("This is the notification date:")
+    //let setDate = notification.fireDate
+    //print(setDate)
+    //print("This is pickedDate")
+    //print(pickedDate)
+    //print("This is from NSUserdefaults")
+    //defaults.setObject(pickedDate, forKey: "savedPickedDate")
+    //let sPD = defaults.objectForKey("savedPickedDate") as? NSDate ?? NSDate()
+    //print(sPD)
+    //print("Ends NSUserDefaults")
+    
+    //let now = NSDate()
+    //print("This is now")
+    //print(now)
+    
+    /*while(sPD != now){
+     sleep(1)
+     }*/
+    
+    /*while(sPD != now){
+     var timer = NSTimer()
+     let delay = 0.1
+     timer = NSTimer.scheduledTimerWithTimeInterval(delay, target: self, selector: #selector(delayedAction), userInfo: nil, repeats: false)
+     }*/
+    
+    /*while(sPD != now){
+     
+     }*/
+    
+    //print("PickedDate works!")
+    
+    /*while(sPD != now){
+     sleep(1)
+     }*/
+    
+    /*while (sPD != now){
+     delay(bySeconds: 1, dispatchLevel: .Background) {
+     while (sPD = now){
+     
+     }
+     }
+     }*/
+    /*while (sPD != now){
+     //var delayTimer = NSTimer()
+     let myTimer : NSTimer = NSTimer.scheduledTimerWithTimeInterval(, target: self, selector: Selector("alarmForDate"), userInfo: nil, repeats: false)
+     
+     }*/
+    
+    //        let defaults = NSUserDefaults.standardUserDefaults()
+    //        let pickedDate = datePicker.date
+    //        defaults.setObject(pickedDate, forKey: "savedPickedDate")
+    //        let sPD = defaults.objectForKey("savedPickedDate") as? NSDate ?? NSDate()
+    //        let now = NSDate()
+    
+    
+    //
+    //        let path = "/System/Library/Audio/UISounds/dtmf-3.caf"
+    //        //let path = NSBundle.mainBundle().pathForResource("tickle", ofType: "mp3")
+    //        let url = NSURL(fileURLWithPath: path)
+    //
+    //        do {
+    //let soundApp = try AVAudioPlayer(contentsOfURL: url)
+    //            self.soundApp = try AVAudioPlayer(contentsOfURL: url)
+    //            self.soundApp.play()
+    //            let testSoundApp = self.soundApp.play()
+    //            print(testSoundApp)
+    //            self.soundApp.numberOfLoops = 3
+    //            print("sounds play now yay")
+    //            let alertController = UIAlertController(title: "Are you up now?", message:"Do you want to turn off the ringtone?", preferredStyle: UIAlertControllerStyle.Alert)
+    //            let cancel = UIAlertAction(title: "Yes", style: UIAlertActionStyle.Cancel, handler: nil)
+    //            alertController.addAction(cancel)
+    //            self.presentViewController(alertController, animated: true, completion: nil)
+    //        }
+    //
+    //
+    //        catch {
+    //            print("There was an error accessing the sound.")
+    //        }
+    //    }
+    
     //Set the alarm based on the data given by user via date picker
-
+    
     func scheduleNotificationForDate(setDate: NSDate) {
         let defaults = NSUserDefaults.standardUserDefaults()
         let pickedDate = datePicker.date
@@ -286,12 +288,12 @@ class AlarmViewController: UIViewController {
             self.soundApp.play()
             let testSoundApp = self.soundApp.play()
             print(testSoundApp)
-            self.soundApp.numberOfLoops = 1
+            self.soundApp.numberOfLoops = -1
             print("sounds play now yay 2")
-//            let alertController2 = UIMutableUserNotification.Action()(title: "Are you up now?", message:"Do you want to turn off the ringtone and return to your settings to turn off Airplane Mode?", preferredStyle: UIAlertControllerStyle.Alert)
-//            let cancel = UIAlertAction(title: "Yes", style: UIAlertActionStyle.Cancel, handler: nil)
-//            alertController2.addAction(cancel)
-//            self.presentViewController(alertController2, animated: true, completion: nil)
+            //            let alertController2 = UIMutableUserNotification.Action()(title: "Are you up now?", message:"Do you want to turn off the ringtone and return to your settings to turn off Airplane Mode?", preferredStyle: UIAlertControllerStyle.Alert)
+            //            let cancel = UIAlertAction(title: "Yes", style: UIAlertActionStyle.Cancel, handler: nil)
+            //            alertController2.addAction(cancel)
+            //            self.presentViewController(alertController2, animated: true, completion: nil)
             
             //self.soundApp.stop()
             let URL: NSURL = NSURL(string: "prefs:root=General")!
@@ -303,14 +305,10 @@ class AlarmViewController: UIViewController {
     }
     
     
-    
-
-//     Mute/unmute
-    //var value: Float = 0.0
-    //AudioSessionSetProperty(kAudioSessionProperty_CurrentHardwareOutputVolume, sizeof(), value)
-    
-    
-    //Alarm tones to come later?
+    @IBAction func stopAlarm(sender: AnyObject) {
+        self.soundApp.stop()
+        
+    }
     
     
 }
